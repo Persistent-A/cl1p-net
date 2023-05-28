8fc4a17e9eda481d5cde5cf53529968e6c3e9796
Persistent-A <apurva.mili@gmail.com>
2023-05-28 19:12:20 -0400

    styled clip panels

--- frontend/src/components/ClipPanel.jsx
+++ frontend/src/components/ClipPanel.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import {
+  useParams,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 import axios from "axios";
-
+import "../styles/clipPanel.css";
 const ClipPanel = () => {
   const { url } = useParams();
+  const [clipExpire, setClipExpire] = useState();
   const [content, setContent] = useState("");
   const [clipTimeOut, setclipTimeOut] = useState("");
   const [showContent, setShowContent] = useState(false);
@@ -16,6 +23,7 @@ const ClipPanel = () => {
         .then((response) => {
           // This code will be executed in case of already existing content on the URL
           setContent(response.data.content);
+          setClipExpire(new Date(response.data.expiresAt));
           setShowContent(true);
         })
         .catch((error) => {
@@ -36,45 +44,94 @@ const ClipPanel = () => {
       .post(`http://localhost:8000/${url}`, userData)
       .then((response) => setShowLink(true));
   };
+
   const handleCopyClick = () => {
     navigator.clipboard.writeText(content);
   };
 
+  const calculateRemainingTime = () => {
+    if (!clipExpire) return 0;
+    const remainingTime = clipExpire.getTime() - Date.now();
+    const remainingMinutes = Math.ceil(remainingTime / 60000);
+    return remainingMinutes;
+  };
+
   return (
-    <div className="clip-panel">
-      {showLink ? (
-        <h1 className="clip-panel__title">cl1ip.net/{url}</h1>
-      ) : showContent ? (
-        <>
-          <h1 className="clip-panel__title">cl1ip.net/{url}</h1>
-          <button onClick={handleCopyClick}>Copy</button>
-          <p className="clip-panel__message">{content}</p>
-        </>
-      ) : (
-        <>
-          <h1 className="clip-panel__title">cl1p.net/{url}</h1>
-          <button onClick={handleCreateClick} className="clip-panel__button">
-            Create cl1p
-          </button>
-          <select
-            onChange={(e) => {
-              setclipTimeOut(e.target.value);
-              console.log(e);
-              console.log(clipTimeOut);
-            }}
-          >
-            <option value="1">Destroy Clip in 1 minute</option>
-            <option value="30">Destroy clip in 30 minutes</option>
-            <option value="45">Destroy clip in 45 minutes</option>
-          </select>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            placeholder="Enter your content..."
-            className="clip-panel__textarea"
-          />
-        </>
-      )}
+    <div className="clip-panel-container">
+      <div className="clip-panel">
+        {showLink ? (
+          <>
+            <Link to="http://localhost:3000/">
+              <h4 className="clip-panel-title">
+                cl1p.net-The internet clipboard
+              </h4>
+            </Link>
+            <p>The cl1p has been created.</p>
+            <p className="clip-panel-title">
+              On any other computer or smart-phone enter in
+              http://localhost:3000/{url} to get your stuff.
+            </p>
+          </>
+        ) : showContent ? (
+          <>
+            <Link to="http://localhost:3000/">
+              <h4 className="clip-panel-title">
+                cl1p.net-The internet clipboard
+              </h4>
+            </Link>
+            <p>
+              Here is your stuff. This cl1p will be deleted in{" "}
+              {calculateRemainingTime()} minutes. Thanks for using cl1p. Tell
+              your friends! <button onClick={handleCopyClick}>Copy</button>
+            </p>
+
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="clip-panel-textarea"
+              rows={27}
+              cols={113}
+            />
+          </>
+        ) : (
+          <div>
+            <Link to="http://localhost:3000/">
+              <h4 className="clip-panel-title">
+                cl1p.net-The internet clipboard
+              </h4>
+            </Link>
+
+            <p>Paste in anything you want</p>
+
+            <button onClick={handleCreateClick} className="clip-panel-button">
+              Create cl1p
+            </button>
+            <select
+              onChange={(e) => {
+                setclipTimeOut(e.target.value);
+                console.log(e);
+                console.log(clipTimeOut);
+              }}
+            >
+              <option value="1">Destroy Clip in 1 minute</option>
+              <option value="30">Destroy clip in 30 minutes</option>
+              <option value="45">Destroy clip in 45 minutes</option>
+            </select>
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="clip-panel-textarea"
+              rows={28}
+              cols={113}
+            />
+            <div>
+              <button onClick={handleCreateClick} className="clip-panel-button">
+                Create cl1p
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
